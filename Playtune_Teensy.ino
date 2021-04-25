ff0c397ff490a3e4dc898dea75b0c2884989141a
Len Shustek <len@shustek.com>
2021-04-25 14:24:58 -0700

    make samples better

--- Playtune_Teensy.ino
+++ Playtune_Teensy.ino
@@ -1,9 +1,11 @@
 // test program for the Teensy version of Playtune
-// (The ABBA "money money money" score has some bugs, but it's ok for test purposes.)
 
 #include "Playtune.h"
-#include "money_2coils.h"
-#include "puttritz.h"
+
+// both samples have issues, as a result of retricting them to
+// 4 channels, but I'm not a great MIDI editor!
+#include "money_2coils.h"    // ABBA'S "Money Money Money"
+#include "puttritz_4chan.h"  // Irving Berline's "Puttin' on the Ritz"
 
 void tune_playnote(byte chan, byte note);
 void tune_stopnote(byte chan);
@@ -14,15 +16,15 @@ void setup(void) {
    tune_initchan(4);
    tune_initchan(5); 
    
-   for (int note=0; note<128; ++note) {
+   for (int note=21; note<109; ++note) { // try all piano notes
     tune_playnote(0,note);
-    delay(500);
+    delay(200);
     tune_stopnote(0);}
    }
 
 void loop(void) {
    delay(1000);
-   tune_playscore(puttritz);
+   tune_playscore(puttritz_4chan);
    delay(2000);
    while (tune_playing) ;
    tune_playscore(money_2coils);
